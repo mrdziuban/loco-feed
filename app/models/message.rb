4092fa13b7dd563095755dfc1c9776bf1eba9fa0
Matt Dziuban <mrdziuban@gmail.com>
2015-07-30 15:18:34 -0400

    Add validations to model.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ActiveRecord::Base
+  validates :text, length: { maximum: 140 }
+  validates_numericality_of :time_to_live, less_than: 24*60*60
+  validates :priority, inclusion: { in: 1..10 }
 end
